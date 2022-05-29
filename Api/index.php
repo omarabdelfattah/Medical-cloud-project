03931c299261a24ca454e0853faf414fabc1b3cd
omarabdelfattah <omar.mero200125@gmail.com>
2022-05-29 07:50:00 +0200

    Login + register for frontendserver

--- Api/index.php
+++ Api/index.php
@@ -2,8 +2,10 @@
 
 // Include CORS headers
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://localhost:3001');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Access-Control-Allow-Headers: X-Requested-With');
+header('Access-Control-Allow-Headers: *');
+header("Access-Control-Allow-Credentials", "true");
 header('Content-Type: application/json');
 
 
