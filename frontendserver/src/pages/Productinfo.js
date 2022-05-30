553ee92d9b7ebdf7e060ff0ac6873be5f205e1de
omarabdelfattah <omar.mero200125@gmail.com>
2022-05-30 02:34:54 +0200

    fix img with details page

--- frontendserver/src/pages/Productinfo.js
+++ frontendserver/src/pages/Productinfo.js
@@ -9,6 +9,7 @@ import axios from "../api/axios";
 const PRODUCT_INFO_URL = "/inventory/detailproduct.php";
 
 function Productinfo() {
+  let { id } = useParams();
 
   const [cookies] = useCookies(['token']);
 const token =  cookies.token;
@@ -16,11 +17,15 @@ const [product_info, setproductInfo] = useState("");
 const [errMsg, setErrMsg] = useState("");
 
 // console.log("hey "+name+ " your token is "+ token );
+
 useEffect(async  () =>  {
 
   try {
     const response = await axios.post(PRODUCT_INFO_URL,
-      JSON.stringify({token:token}),
+      JSON.stringify({
+            token:token, 
+            id:id
+          }),
       {
         headers: { 
           'Content-Type': 'application/json',
@@ -36,7 +41,7 @@ useEffect(async  () =>  {
     if (status == 'success') {
 
       setproductInfo(product_info);
-      console.log(product_info);
+
     }else{
       if(response?.data?.response?.msg){
         setErrMsg(response?.data?.response?.msg);
@@ -58,54 +63,36 @@ useEffect(async  () =>  {
 
 }, []);
 
-  let { id } = useParams();
 
   
   return (
     <div className="container mt-3">
       <PageNav />
 
-      {/* <div className="row">
+      <div className="row">
         <div className="col-8">
           <h1 style={{ textDecoration: "underline #639DC3 10px" }}>
-            {product[0].title}
+            {product_info.name}
           </h1>
-          <h3 className="mt-4">Composition :</h3>
+          <br />
+          <h3>Price:</h3>
+        <p>
+          <big>{product_info.price} EGP</big>
+        </p>
+        <h3>Customer rating :</h3>
+        <p>
+          <big>{product_info.rating} / 5</big>
+        </p>
+          <h3 className="mt-4">Description :</h3>
           <p>
-            <big>{product[0].composition}</big>
-          </p>
-          <h3>Indcation And Usage :</h3>
-          <ul>
-            <li>
-              <p>
-                <big>{product[0].usage1}</big>
-              </p>
-            </li>
-            <li>
-              <p>
-                <big>{product[0].usage2}</big>
-              </p>
-            </li>
-            <li>
-              <p>
-                <big>{product[0].usage3}</big>
-              </p>
-            </li>
-          </ul>
+            <big>{product_info.desc}</big>
+          </p>       
         </div>
         <div className="col-4">
-          <img src={product[0].image} className="drug" style={{ width: "90%" }} />
+          <img src={product_info.img} className="drug" style={{ width: "90%" }} />
         </div>
       </div>
       <div className="row">
-        <h3>Adverse Effects:</h3>
-        <p>
-          <big>{product[0].Adverse_Effects}</big>
-        </p>
-        <h3>Dosage And Administration :</h3>
-        <p>
-          <big>{product[0].Dosage_Administration}</big>
-        </p>
         <a href={"/Order/" + id} className="m-3" style={{ float: "right" }}>
           <button
             className="btn btn-lg btn-primary"
@@ -114,7 +101,7 @@ useEffect(async  () =>  {
             Order Now
           </button>
         </a>
-      </div> */}
+      </div>
     </div>
   );
 }
