f38d7e13812bd07aa34d00edc9f124e5e35eb36e
omarabdelfattah <omar.mero200125@gmail.com>
2022-06-02 01:51:17 +0200

    build

--- frontendserver/src/api/axios.js
+++ frontendserver/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http:///localhost:9090/",
+    baseURL: "http:///0.0.0.0:9090/",
     headers: {
         "Content-Type": "application/json",
     },
